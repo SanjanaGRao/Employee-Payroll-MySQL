0808e157aa6470d47f7a4fed198fedc912f4bf2d
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-28 08:45:16 +0530

    Modified: Ability to check if a year is leap year or not

--- LeapYearCheck.js
+++ LeapYearCheck.js
@@ -1,7 +1,17 @@
-var year = 2020;
+var year;
+const readline = require('readline');
 
-//Logic to check if its a Leap year or not
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter a year: ', (year) => {
+  //Logic to check if its a Leap year or not
 if( ((year % 4 == 0) && (year%100!=0)) ||(year % 400 == 0) )
-	console.log(" Year " + year + " is a leap year.");
+    console.log(" Year " + year + " is a leap year.");
 else
-	console.log(" Year " + year + " is not a leap year.");
+    console.log(" Year " + year + " is not a leap year.");
+  rl.close();
+});
+
